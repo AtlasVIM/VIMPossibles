b085c7afcbfd3c672fa3d7c40fbc98524d3401bf
ATLAS <caldasmartim@gmail.com>
2023-12-20 16:41:33 +0000

    added flipping character cards

--- js/views/main-view.js
+++ js/views/main-view.js
@@ -3,14 +3,25 @@ const internals = {}
 const externals = {}
 
 internals.createSpyCard = function(spy) {
-    return `<img width=200 height=250 src= "${spy.img}"/>`
+    return `<span class="spy-card"> 
+    <div class= "spy-card-inner">
+    <div class="spy-card-front">
+    <img width=200 height=250 src= "${spy.img}"/>
+    </div>
+    <div class="spy-card-back">
+    <h1> ${spy.name} </h1>
+    <p> ${spy.specialty}</p>
+    <p> ${spy.description}</p>
+    </div>
+    </div>
+    </span>`
 }
 
 internals.renderSpy = function(spy) {
     console.log(spy)
     internals.spyCard = internals.createSpyCard(spy);
     console.log(internals.spyCard);
-    $('#app').append(internals.spyCard);
+    $('#card-container').append(internals.spyCard);
 
 }
 
